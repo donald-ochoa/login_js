ee7db1f84be2aeeed321339c295e14eae23f06f3
unknown <donald_ochoa_m@yahoo.es>
2020-05-28 00:27:23 -0600

    new file:   index.html 	new file:   js/login.js 	new file:   styles/style.css

---
+++ js/login.js
@@ -0,0 +1,60 @@
+
+class Usuario {
+    
+    obtener(){    
+        var getCorreo = document.getElementById("correo").value;
+        var getPass = document.getElementById("pass").value;
+        
+        this.p(getCorreo, getPass);        
+    }   
+    
+    p = function (getCorreo, getPass) {
+
+        let secretEmail = "correo@example.com";
+        let secretPassword = 1234;
+
+        const promesa = new Promise((resolve, reject)=>{                       
+
+                if (secretEmail == getCorreo  && secretPassword == getPass) { // Aqui validan los parametros obtenidos 
+                        resolve("Procesamiento bien");                  
+                    
+                        this.getTodos();
+            
+                } else {
+                        reject("Algo estuvo mal");
+                    
+                }
+            });
+
+            promesa.then((mensaje) => {
+                    alert("Bienvenido nuevamente: " + secretEmail);
+                
+                    // llamar funcion asincrona
+                    getTodos();
+                
+            }).catch((error)=>{
+                alert("Usuario no valido");               
+            });
+
+        }        
+    }
+
+    //Función asíncrona.
+    getTodos = async () => {
+        var datos = await axios.get("https://randomuser.me/api/?results=5")
+        .then(result =>{
+            var correo = result.data.results[0].email;
+            
+            console.log("Correo con función asíncrona: " + correo); //mostrar correo obtenido mediante la función asíncrona.
+            alert("Correo con función asíncrona: " + correo); 
+        })
+        .catch(error =>{
+            console.log(error);        
+        }); 
+    };
+
+//Creando la 
+var datosUser = new Usuario;
+
+
+
